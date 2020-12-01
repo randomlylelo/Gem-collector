94043a1617208602241cfa7d9ba93b9a5edcdd58
randomlylelo <lzhang.0693@gmail.com>
2020-12-01 16:40:08 -0500

    add assets

--- src/game/game.js
+++ src/game/game.js
@@ -5,13 +5,29 @@ const config = {
   width: 800,
   height: 600,
   parent: 'game',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
   backgroundColor: '#18216D',
 };
 
-class PuzzleGame extends Phaser.Game {
-  constructor(config) {
-    super(config);
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super('Game');
+  }
+
+  preload() {
+    this.load.image('black', 'game/assets/black.png');
+  }
+
+  create() {
+    this.add.image(400, 300, 'black');
   }
 }
 
-const game = new PuzzleGame(config);
+config.scene = GameScene
+
+new Phaser.Game(config);
