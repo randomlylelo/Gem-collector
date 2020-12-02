38eb1c5e5690ea513a0135d54dea9fd123be29a6
randomlylelo <lzhang.0693@gmail.com>
2020-12-01 20:54:03 -0500

    Started working on row deletion

--- src/game/game.js
+++ src/game/game.js
@@ -1,5 +1,11 @@
 require('phaser');
 
+// How much to shift the grid of blocks
+const gridDisplacement = {
+  x: 100,
+  y: 100,
+};
+
 const config = {
   title: 'FBLA',
   width: 800,
@@ -37,17 +43,64 @@ class GameScene extends Phaser.Scene {
     for (let i = 0; i < rows; i++) {
       level[i] = [];
       for (let j = 0; j < cols; j++) {
+        const xCord = 64 * i + 32;
+        const yCord = 64 * j + 32;
+
         // 0 - 3 (inclusive)
         const number = Math.floor(Math.random() * 4);
 
-        // change the +100 to move the grid.
-        level[i][j] = this.add
-          .image(64 * i + 32 + 100, 64 * j + 32 + 100, gems[number])
+        const sprite = this.add
+          .sprite(
+            xCord + gridDisplacement.x,
+            yCord + gridDisplacement.y,
+            gems[number]
+          )
           .setInteractive();
-        this.input.setDraggable(level[i][j]);
+
+        this.input.setDraggable(sprite);
+
+        level[i][j] = { xCord, yCord, color: gems[number], sprite };
       }
     }
 
+    this.input.on('pointerdown', (pointer, gameObject) => {
+      // x = col (x-axis, straight up)
+      // y = row
+
+      const findDuplicates = (sprite) => {
+        const row = (sprite.x - gridDisplacement.x - 32) / 64;
+        const col = (sprite.y - gridDisplacement.y - 32) / 64;
+
+        // Find all neighboring sprites with same color and call recursivly.
+    
+        if (level[row][col - 1] === undefined) {
+          level[row][col].sprite.destroy();
+          level[row][col].sprite = null;
+          console.log(level);
+          return;
+        }
+        // Just Check if one side is not defined^^
+        // TODO: ^^
+        // As long as one side of not defined then contiune with program... so wrap everything in if loop.
+
+        // Check Top Color
+        console.log(col);
+        console.log(level);
+        if (level[row][col].color === level[row][col - 1].color) {
+          findDuplicates(level[row][col - 1].sprite);
+        }
+
+        level[row][col].sprite.destroy();
+        level[row][col].sprite = null;
+        console.log(level);
+      };
+
+      findDuplicates(gameObject[0]);
+
+      // if (gameObject[0]) {
+      //   gameObject[0].destroy();
+      // }
+    });
   }
 }
 
